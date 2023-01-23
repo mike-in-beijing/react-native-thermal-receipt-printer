d74ac2f60c7ebe14cf983b2d3b123fbf57e961e7
mainxiang <majinxiang@huimin100.cn>
2023-01-23 23:06:57 +0800

    添加字号

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,7 +4,7 @@ export interface PrinterOptions {
     cut?: boolean;
     tailingLine?: boolean;
     encoding?: string;
-    fontSize?: number;
+    fontSize?: string;
 }
 export interface IUSBPrinter {
     device_name: string;
